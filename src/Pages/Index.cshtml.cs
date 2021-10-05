59c1d5cc382280a93c0b5729c9570671b699f716
andydandy77 <david.j.anderson94@gmail.com>
2021-10-04 18:59:42 -0700

    Added name Dave Anderson comment to index.cshtml.cs.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,6 +8,10 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
+    /// <summary>
+    /// Dave Anderson
+    /// </summary>
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
