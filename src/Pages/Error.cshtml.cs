82c55b40abfa3242985d8af4a1a0f648424303d2
roopinij17 <roopinijreddy@gmail.com>
2021-10-11 19:36:15 -0700

    Adding comment to Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
+//Adding a comment for Roopini
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
