ab2fc09e1145cb16a0b49bfc6bbac5e54833b2a5
Denis <denisrajic7@gmail.com>
2021-10-11 19:28:42 -0700

    Added comments to Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-
+// Adding comment that will be synced
 namespace ContosoCrafts.WebSite.Pages
 {
 
