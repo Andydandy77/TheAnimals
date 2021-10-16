6be28604300a6c68016f21e34594de66b8f810a5
Denis <denisrajic7@gmail.com>
2021-10-15 17:59:54 -0700

    Updated and added fields for ProdcutModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -6,14 +6,18 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         public string Id { get; set; }
-        public string Maker { get; set; }
-        
+        public string Category { get; set; }
+        public string Restaurant { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
         [JsonPropertyName("img")]
         public string Image { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
+        public string[] Reviews { get; set; }
+        public float Price { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
