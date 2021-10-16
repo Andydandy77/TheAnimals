74557c98770c6fdb95b77f58e1aefde722fcc6c7
Denis <denisrajic7@gmail.com>
2021-10-16 11:13:25 -0700

    Added OnGet method to Create.cshtml.cs

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -4,13 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Models;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Create Page
+    /// </summary>
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        // Data middle tier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public CreateModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        // Data to show
+        public ProductModel Product;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public IActionResult OnGet()
+        {
+            Product = ProductService.CreateData();
+
+            // Redirect the webpage to the Update page populated with the data so the user can fill in the fields
+            return RedirectToPage("./Update", new { Id = Product.Id });
         }
     }
 }
