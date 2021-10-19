3041276bee816115345c64921c488b90bec3637e
andydandy77 <david.j.anderson94@gmail.com>
2021-10-18 23:29:21 -0700

    Finished commenting index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,17 +9,19 @@ namespace ContosoCrafts.WebSite.Pages
 {
 
     /// <summary>
-    /// Dave Anderson
-    /// Roopini Jayakumar
-    /// Denis Rajic
-    /// Gerry Wang
+    /// Model for the Index page
+    /// Contains functionality to access all Products
     /// </summary>
-    ///
-
     public class IndexModel : PageModel
     {
+        // Ilogger for write to the console and debugging
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Default Constructor, sets the ILogger and ProductService
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
@@ -27,9 +29,16 @@ namespace ContosoCrafts.WebSite.Pages
             ProductService = productService;
         }
 
+
+        // ProductService to access data about products
         public JsonFileProductService ProductService { get; }
+        // An IEnumerable that allows us to iterate over the Products
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        /// <summary>
+        /// When this page loads this sets Products to the Products returned by
+        /// ProductService.GetAllData()
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetAllData();
