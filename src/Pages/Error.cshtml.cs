21308c2c6b084ce8afdb41e1a74094ad3d6ff623
roopinij17 <roopinijreddy@gmail.com>
2021-10-19 14:43:58 -0700

    Added Cooment to Error.cshtml.cs file

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -1,29 +1,33 @@
 using System.Diagnostics;
-// Adding comment
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
-//Adding a comment for Roopini
+
 {
+    //returns Cache-Control: no-store,no-cache
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        // Id for the Request
         public string RequestId { get; set; }
 
+        //returns True if RequestID is not Null
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
 
+        //Represents a type used to perform logging. 
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        //handles GET exceptions
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
-}
+}   
