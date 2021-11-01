7a80c5f61798a871d555ed50f0bbf85b9881346d
andydandy77 <david.j.anderson94@gmail.com>
2021-10-31 22:33:50 -0700

    Added comments to Index.cshtml.Tests.cs

--- UnitTests/Pages/Product/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -8,13 +8,20 @@ using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Index
 {
+    /// <summary>
+    /// IndexTests provides unit tests for Index.cshtml.cs
+    /// </summary>
     public class IndexTests
     {
         #region TestSetup
         public static PageContext pageContext;
 
+        // This is our model that we test OnGet
         public static IndexModel pageModel;
 
+        /// <summary>
+        /// This initializes page model from TestHelper.ProductService
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -26,6 +33,9 @@ namespace UnitTests.Pages.Product.Index
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
+        /// This tests that Index's OnGet returns the list of dishes
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products()
         {
