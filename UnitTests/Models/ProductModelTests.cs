e2f8971ed8cacbd810d28b9693e0331b82bf8ae0
Gerry Wang <72587411+WaterProof1898@users.noreply.github.com>
2021-11-02 14:52:11 -0700

    Added unit test ToString_Valid_Should_Return_Product_As_String() to ProductModelTests.cs

--- UnitTests/Models/ProductModelTests.cs
+++ UnitTests/Models/ProductModelTests.cs
@@ -1,8 +1,57 @@
 ﻿using System;
 
-public class Class1
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Pages.Product.Delete
 {
-	public Class1()
-	{
-	}
+    /// <summary>
+    /// Tests ProductModels
+    /// </summary>
+    public class ProductModelTests
+    {
+        #region TestSetup
+        /// <summary>
+        /// Initialize a ProductModel with the TestHelper
+        /// </summary>
+        public static ProductModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ProductModel()
+            {
+            };
+        }
+
+        #endregion TestSetup 
+
+        #region ToString
+        /// <summary>
+        /// Tests that ToString() method should return the string representation of the dish
+        /// </summary>
+        [Test]
+        public void ToString_Valid_Should_Return_Product_As_String()
+        {
+            // Arrange
+            var expected = "{\"Id\":null,\"Category\":null,\"Restaurant\":null,\"Address\":null,\"City\":null,\"State\":null,\"img\":null,\"Url\":null,\"Title\":null,\"Cuisine\":null,\"Description\":null,\"Ratings\":null,\"Reviews\":null,\"Price\":0}";
+            // Act
+            var result = pageModel.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+
+        }
+        #endregion ToString
+
+        
+
+
+    }
 }
