2f474afde75c62579d78bfac87bb3c9c5f10b1d1
roopinij17 <roopinijreddy@gmail.com>
2021-11-01 17:00:38 -0700

    Added comments to Error.cshtml.Tests.cs

--- UnitTests/Pages/Error.cshtml.Tests.cs
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -10,19 +10,29 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Error
 {
+    /// <summary>
+    /// Unit Test for Error.cshtml.cs
+    /// </summary>
     public class ErrorTests
     {
         #region TestSetup
+        //Testing OnGet
         public static ErrorModel pageModel;
 
+        /// <summary>
+        /// Initializing page model with TestHelper
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            //Intializing variable for ILogger<ErrorModel>
             var MockLoggerDirect = Mock.Of<ILogger<ErrorModel>>();
 
             pageModel = new ErrorModel(MockLoggerDirect)
             {
+                //Initializing PageContext with TestHelper
                 PageContext = TestHelper.PageContext,
+                //Initializing TempData with TestHelper
                 TempData = TestHelper.TempData,
             };
         }
@@ -30,6 +40,9 @@ namespace UnitTests.Pages.Error
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
+        /// Tests if OnGet returns a Valid RequestID
+        /// </summary>
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
@@ -49,6 +62,9 @@ namespace UnitTests.Pages.Error
             Assert.AreEqual(activity.Id, pageModel.RequestId);
         }
 
+        /// <summary>
+        /// Tests if OnGet returns a trace
+        /// </summary>
         [Test]
         public void OnGet_InValid_Activity_Null_Should_Return_TraceIdentifier()
         {
