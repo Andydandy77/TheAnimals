6105296cbd9f6af662c54d04653d8d51b29f4005
Gerry Wang <72587411+WaterProof1898@users.noreply.github.com>
2021-11-09 13:58:33 -0800

    Added spaces to Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -14,7 +14,9 @@ namespace ContosoCrafts.WebSite
         /// </summary>
         public Startup(IConfiguration configuration)
         {
+
             Configuration = configuration;
+        
         }
 
         public IConfiguration Configuration { get; }
@@ -25,31 +27,38 @@ namespace ContosoCrafts.WebSite
         /// </summary>
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddHttpClient();
             services.AddControllers();
             services.AddTransient<JsonFileProductService>();
+        
         }
 
         /// <summary>
         ///  This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        /// </summary>
-        
+        /// </summary> 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
+            
             }
             else
             {
+
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
