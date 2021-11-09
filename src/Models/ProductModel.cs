bdf2df8e3bd5bbc3ce99ac419ee866008fb6b7f9
Gerry Wang <72587411+WaterProof1898@users.noreply.github.com>
2021-11-09 13:43:13 -0800

    Added spaces to variables on ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -12,33 +12,47 @@ namespace ContosoCrafts.WebSite.Models
     {
         // Id of dish
         public string Id { get; set; }
+
         // Type of dish
         public string Category { get; set; }
+
         // Restaurant where dish is sold
         public string Restaurant { get; set; }
+
         // Street address of restaurant
         public string Address { get; set; }
+
         // City of restaurant
         public string City { get; set; }
+
         // State of restaurant
         public string State { get; set; }
+
         // Image of dish
         [JsonPropertyName("img")]
         public string Image { get; set; }
+
         // Url for website of restaurant
         public string Url { get; set; }
+
         // Name of the dish at restaurant
         public string Title { get; set; }
+
         // Cuisine that the dish belongs to 
         public string Cuisine { get; set; }
+
         // Description of dish
         public string Description { get; set; }
+
         // User ratings of dish
         public int[] Ratings { get; set; }
+
         // User reviews of dish
         public string[] Reviews { get; set; }
+
         // Price of dish
         public float Price { get; set; }
+
 
         /// <summary>
         /// Returns string representation of the dish
@@ -57,8 +71,11 @@ namespace ContosoCrafts.WebSite.Models
             // If there are ratings, add all ratings and divide them by nuber of votes
             if (Ratings != null)
             {
+
                 int voteCount = Ratings.Length;
+                
                 rating = Ratings.Sum() / voteCount;
+            
             }
 
             // Return average rating of dish
@@ -76,6 +93,7 @@ namespace ContosoCrafts.WebSite.Models
         public bool HasSearchQuery(string search)
         {
             search = search.ToLower();
+            
             if (Category != null)
             {
                 if (Category.ToLower().Contains(search))
