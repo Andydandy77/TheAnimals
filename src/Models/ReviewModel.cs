48f54c5f80067260e60685d976720b3880b63dd2
andydandy77 <david.j.anderson94@gmail.com>
2021-11-14 21:59:29 -0800

    Added ReviewModel.cs to the Models folder

---
+++ src/Models/ReviewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Reviews entered by the user about the Dish
+    /// </summary>
+    public class ReviewModel
+    {
+        // The ID for this comment, use a Guid so it is always unique
+        public string Id { get; set; } = System.Guid.NewGuid().ToString();
+
+        // The Comment
+        public string Review { get; set; }
+    }
+}
