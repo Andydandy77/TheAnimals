ef749145bacfa2f2f88cc07fc95b4400d0ac551a
Denis <denisrajic7@gmail.com>
2021-11-28 10:03:36 -0800

    Added info for users in UserModel.cs

---
+++ src/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
