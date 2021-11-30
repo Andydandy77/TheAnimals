a75753997ab691d4cd666d50a90ff7cf0d70ab34
Denis <denisrajic7@gmail.com>
2021-11-29 20:31:06 -0800

    Added comments to UserModel.cs

--- src/Models/UserModel.cs
+++ src/Models/UserModel.cs
@@ -5,10 +5,18 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Represents a user object
+    /// </summary>
     public class UserModel
     {
+        // Unique user ID
         public string Id { get; set; }
+
+        // Individual user's username
         public string Username { get; set; }
+
+        // The user's password
         public string Password { get; set; }
     }
 }
