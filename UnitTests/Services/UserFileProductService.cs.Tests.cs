ccd792e2ef90f4ef4d6f25f0ee35a97cd13bfcc8
Denis <denisrajic7@gmail.com>
2021-11-29 21:29:41 -0800

    Added tests for GetUser and CreateUser in UserFileProductService.cs.Tests.cs

---
+++ UnitTests/Services/UserFileProductService.cs.Tests.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Services.Models.UserServiceTests
+{
+    /// <summary>
+    /// Tests the Product Service file
+    /// </summary>
+    public class UserFileProdcutService
+    {
+        #region TestSetup
+
+        /// <summary>
+        /// Initializes Tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        #region CreateUser
+        /// <summary>
+        /// Tests that new users are added to database
+        /// </summary>
+        [Test]
+        public void CreateUser_Valid_Should_Add_to_JsonUserFile()
+        {
+            // Arrange
+            var username = "aaaaaaaaa";
+            var password = "password";
+
+            // Act
+            TestHelper.UserService.CreateUser(username, password);
+            var data = TestHelper.UserService.GetUser(username);
+            // Assert
+            Assert.AreEqual(data.Username, username);
+
+
+        }
+
+        #endregion CreateUser
+
+        #region GetUser
+        [Test]
+        public void GetUser_Invalid_Should_Return_Null()
+        {
+            // Arrange
+            var username = "asdfaafsdfa";
+
+            // Act
+            var data = TestHelper.UserService.GetUser(username);
+
+            // Assert
+            Assert.AreEqual(null, data);
+
+        }
+        #endregion GetUser
+
+    }
+}
