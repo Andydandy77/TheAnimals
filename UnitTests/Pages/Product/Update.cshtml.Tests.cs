dd561b32e61d50615028ccf92552f6e4c2483b6d
David Anderson <danderson2@seattleu.edu>
2021-11-30 18:42:52 -0800

    Added Unit tests to Update.cshtml.Tests.cs to test the functionality of the validate input function

--- UnitTests/Pages/Product/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -100,5 +100,299 @@ namespace UnitTests.Pages.Product.Update
             Assert.AreEqual(false, pageModel.ModelState.IsValid);
         }
         #endregion OnPost
+
+        #region ValidateInput
+
+        [Test]
+        public void Invalid_Title_Length_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 10,
+                City = "city",
+                State = "WA",
+                Title = "titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north ave"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_Description_Length_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 10,
+                City = "city",
+                State = "WA",
+                Title = "title",
+                Description = "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north ave"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_Restaurant_Length_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurantrestaurantrestaurantrestaurant",
+                Price = 10,
+                City = "city",
+                State = "WA",
+                Title = "title",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north ave"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_City_Length_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 10,
+                City = "citycitycitycitycitycitycitycitycitycitycitycitycitycitycitycity",
+                State = "WA",
+                Title = "title",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north ave"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_State_Length_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 10,
+                City = "city",
+                State = "WAA",
+                Title = "title",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north ave"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_Cuisine_Length_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 10,
+                City = "city",
+                State = "WA",
+                Title = "title",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexicanmexicanmexicanmexicanmexicanmexicanmexicanmexican",
+                Address = "189 north ave"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_Address_Length_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 10,
+                City = "city",
+                State = "WA",
+                Title = "title",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north ave189 north ave189 north ave189 north ave189 north ave189 north ave189 north ave189 north ave"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_URL_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 10,
+                City = "city",
+                State = "WA",
+                Title = "title",
+                Description = "description",
+                Url = "https:/ww.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_Low_Price_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = -1,
+                City = "city",
+                State = "WA",
+                Title = "title",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Invalid_High_Price_Does_Not_Return_Index()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "sandwich",
+                Category = "category",
+                Restaurant = "restaurant",
+                Price = 1000,
+                City = "city",
+                State = "WA",
+                Title = "title",
+                Description = "description",
+                Url = "https://www.google.com",
+                Image = "https://pbs.twimg.com/profile_images/1022058466708516864/xvWejEyc.jpg",
+                UberEats = "ubereats",
+                Cuisine = "mexican",
+                Address = "189 north"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, result);
+        }
+
+        #endregion ValidateInput
     }
 }
